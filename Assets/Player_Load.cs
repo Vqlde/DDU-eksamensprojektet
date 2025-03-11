42cb41592ba3999371914f4fc241a0b0b1c43817
Vqlde <88985086+Vqlde@users.noreply.github.com>
2025-03-11 09:43:06 +0100

    Playerload changes

--- Assets/Player_Load.cs
+++ Assets/Player_Load.cs
@@ -6,21 +6,18 @@ public class Player_Load : MonoBehaviour
 {
     void Start()
     {
-        // Get the selected character from PlayerPrefs
         string character = PlayerPrefs.GetString("SelectedCharacter", "brute");
 
-        // Load the prefab from the Resources/Characters folder
         GameObject loadedPrefab = Resources.Load<GameObject>("Characters/" + character);
 
         if (loadedPrefab != null)
         {
-            // Instantiate the character at position (0, 0, 0)
             Instantiate(loadedPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-            Debug.Log($"Character '{character}' spawned successfully.");
+            Debug.Log($"Character '{character}' blev fundet.");
         }
         else
         {
-            Debug.LogError($"Character '{character}' not found in Resources/Characters folder!");
+            Debug.LogError($"Character '{character}' blev ikke fundet!");
         }
     }
 }
